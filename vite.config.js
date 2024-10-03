6fec819ea154e9bec7cf80653d7f9bfbd12cc438
nicolasm1605 <nicolas.moreno@utp.edu.co>
2024-10-02 23:50:53 -0500

    deploy front

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '//StockPro'
+  base: 'https://nicolasm1605.github.io/StockPro_landing'
 })
